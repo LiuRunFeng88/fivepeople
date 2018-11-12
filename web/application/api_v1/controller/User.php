a1b0a52e866b66f01e0fa3a1a68344246d86bc23
liurunfeng <18826583684@dionly.com>
2018-11-12 15:01:39 +0800

    init address

--- web/application/api_v1/controller/User.php
+++ web/application/api_v1/controller/User.php
@@ -8,6 +8,8 @@ namespace app\api_v1\controller;
 
 
 use app\common\controller\BaseController;
+use app\common\model\ErrorCode;
+use think\Db;
 
 class User extends BaseController {
 
@@ -21,5 +23,66 @@ class User extends BaseController {
             return err_return($e->getCode(),$e->getMessage());
         }
     }
+
+    //添加收货地址
+    public function add_address(){
+        Db::startTrans();
+        try{
+            if (empty($this->_data['name']) || empty($this->_data['area']) || empty($this->_data['address'])){
+                throw new \Exception(ErrorCode::formatErrorMsg(ErrorCode::INPUT_ERROR),ErrorCode::INPUT_ERROR);
+            }
+            if (!preg_match('/^1[0-9]{10}$/', $this->_data['mobile'])){
+                throw new \Exception(ErrorCode::formatErrorMsg(ErrorCode::ERROR_LOGIN_MOBILE),ErrorCode::ERROR_LOGIN_MOBILE);
+            }
+            $is_default = $this->_data['is_default']??0;
+            $address = array();
+            $address['mobile'] = $this->_data['mobile'];
+            $address['area']=$this->_data['area'];
+            $address['address']=$this->_data['address'];
+            $address['name'] =$this->_data['name'];
+            $address['created_at']=time();
+            //是否设为默认地址
+            if ($is_default){
+                //修改之前的默认地址
+                \app\common\model\UsersAddress::where(['user_id'=>$this->_user['id'],'status'=>1])->update(['status'=>0]);
+                 $address['status']=1;
+            }else{
+                $address['status'] =0;
+            }
+            \app\common\model\UsersAddress::create($address);
+            Db::commit();
+            return suc_return();
+        }catch (\Exception $e){
+            Db::rollback();
+            return err_return($e->getCode(),$e->getMessage());
+        }
+    }
+
+    //修改收货地址
+    public function edit_address(){
+        try{
+            if (empty($this->_data['id']) || empty($this->_data['name']) || empty($this->_data['area']) || empty($this->_data['address'])){
+                throw new \Exception(ErrorCode::formatErrorMsg(ErrorCode::INPUT_ERROR),ErrorCode::INPUT_ERROR);
+            }
+            if (!preg_match('/^1[0-9]{10}$/', $this->_data['mobile'])){
+                throw new \Exception(ErrorCode::formatErrorMsg(ErrorCode::ERROR_LOGIN_MOBILE),ErrorCode::ERROR_LOGIN_MOBILE);
+            }
+            $address = \app\common\model\UsersAddress::get($this->_data['id']);
+            if (empty($address)){
+                throw new \Exception(ErrorCode::formatErrorMsg(ErrorCode::INPUT_ERROR),ErrorCode::INPUT_ERROR);
+            }
+            $address->name = $this->_data['name'];
+            $address->area = $this->_data['area'];
+            $address->address = $this->_data['address'];
+            $address->mobile = $this->_data['mobile'];
+            $address->update_at = time();
+            $address->save();
+            return suc_return();
+        }catch (\Exception $e){
+            return err_return($e->getCode(),$e->getMessage());
+        }
+    }
+
+
 
 }
---
+++ web/application/common/model/UsersAddress.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/11/7
+ * Time: 11:05
+ */
+/**
+ * Created by 刘先生
+ * 2018/11/7 11:05
+ */
+
+namespace app\common\model;
+
+
+use think\Model;
+
+class UsersAddress extends Model {
+
+    protected $table = 'users_address';
+
+    public static $err_code;
+
+
+}
