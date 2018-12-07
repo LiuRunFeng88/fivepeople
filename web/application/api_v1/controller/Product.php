672c8b69b02820135ce05ba1320454c9fc702a56
liurunfeng <18826583684@dionly.com>
2018-12-07 15:32:02 +0800

    product_list

--- web/application/api_v1/controller/Product.php
+++ web/application/api_v1/controller/Product.php
@@ -25,6 +25,50 @@ class Product extends BaseController {
             $this->e = $e;
         }
     }
+    //商品列表
+    public function product_list(){
+        try{
+            $this->_checkAppRequest();
+            $whereArr = array();
+            //筛选
+            if (!empty($this->_data['keyword'])){
+                $keyword = $this->_data['keyword'];
+                $whereArr['name'] = ['LIKE',"%$keyword%"];
+            }
+            if (!empty($this->_data['classify_id'])){
+                $whereArr['classify_id'] = $this->_data['classify_id'];
+            }
+            $this->_data['type'] = $this->_data['type'] ?? 1;
+            $this->_data['page'] = $this->_data['page'] ?? 1;
+            //排序
+            if ($this->_data['type'] == 1){ //综合
+                $orderArr = [];
+            }elseif ($this->_data['type'] == 2){ //销量
+                $orderArr = ['sale_quantity'=>'DESC'];
+            }elseif ($this->_data['type'] == 3){ //价格
+                $orderArr = ['selling_price'=>'DESC'];
+            }else{ //新品
+                $orderArr = ['release_at'=>'DESC'];
+            }
+            //获取商品列表
+            $product_list = \app\common\model\Product::where($whereArr)->order($orderArr)->paginate(null,false,['page'=>$this->_data['page']]);
+            $data = $product_list->toArray();
+            $data = $data['data'];
+            foreach ($data as $index=>$product){
+                $data[$index]['pics'] = explode(',',$product['pics']);
+                $data[$index]['medias'] = explode(',',$product['medias']);
+            }
+            $result = [
+                'total' => $product_list->total(),
+                'per_page' => config('paginate.list_rows'),
+                'current_page' => $product_list->currentPage(),
+                'list'=>$data
+            ];
+            return suc_return($result);
+        }catch (\Exception $e){
+            return err_return($e->getCode(),$e->getMessage());
+        }
+    }
 
     //商品详情
     public function product_details(){
